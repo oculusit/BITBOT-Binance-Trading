a5fbf6d42aec1c2c6983b9c4678619029471c030
oculusit <oculus@oculus.it>
2022-01-26 10:49:50 +0100

    Update BITBOT-Trading-Binance-NewConcept.py

--- BITBOT-Trading-Binance-NewConcept.py
+++ BITBOT-Trading-Binance-NewConcept.py
@@ -27,7 +27,7 @@ from binance import Client, ThreadedWebsocketManager, ThreadedDepthCacheManager
 
 
 ######## RELEASE VERSION ##############################################
-rel = "0.8.13 Binance Trading ** TEST NEW CONCEPT **"
+rel = "0.8.14 Binance Trading ** TEST NEW CONCEPT **"
 
 #######################################################################
 ######## CONFIGURATION VARIABLES ######################################
