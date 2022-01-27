5b9d6cfabfbd70828de9e4c410a9ff35648d654e
oculusit <oculus@oculus.it>
2022-01-27 18:34:14 +0100

    Update bot.sh

--- bot.sh
+++ bot.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
-while [ $? -eq 0 ]
+while :
 do
   ./BITBOT-Trading-Binance-NewConcept.py
+  if $? -eq 1
+  then
+   echo "Ended by AUTO STOP FUNCTION"
+   break
+  fi
 done
-echo "Ended by AUTO STOP FUNCTION"
+echo "Ended by ERROR!!"
