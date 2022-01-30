ece022596947134a99f824ddf7295bfc89da97b0
Alessandro <oculus@oculus.it>
2022-01-30 11:41:29 +0100

    Update BITBOT-Trading-Binance-NewConcept.py

--- BITBOT-Trading-Binance-NewConcept.py
+++ BITBOT-Trading-Binance-NewConcept.py
@@ -28,7 +28,7 @@ from binance import Client, ThreadedWebsocketManager, ThreadedDepthCacheManager
 
 
 ######## RELEASE VERSION ##############################################
-rel = "0.10.011 Binance Trading ** TELEGRAM INTEGRATION * Commissions Calculation **"
+rel = "0.10.012 Binance Trading ** TELEGRAM INTEGRATION * Commissions Calculation **"
 
 #######################################################################
 ######## CONFIGURATION VARIABLES ######################################
